d76569dd1a09aac1c0ce3f27ce5902ec55179e71
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-06-13 19:18:45 +0200

    early version with local storage save

--- src/js/details.js
+++ src/js/details.js
@@ -1,13 +1,29 @@
 const API_KEY = 'e7c806d7ce9bbdf1ef93bebcabbfe0f1';
+const modalBackdrop = document.querySelector('.backdrop');
+const btnClose = document.querySelector('.modal__btnClose');
+const btnWatched = document.querySelector('.modal__btnWatched');
+const btnQueued = document.querySelector('.modal__btnQueue');
+const modalImg = document.querySelector('.modal__moviePoster');
+const modalTitle = document.querySelector('.modal__title');
+const modalAbout = document.querySelector('.modal__about');
 
+let movieId;
+let queuedFilmsArray = [];
+let watchedFilmsArray = [];
 function detailsHandler(clickedMovie) {
-  console.log(clickedMovie.target);
-  const movieId = clickedMovie.target.parentNode.id;
-  console.log(movieId);
-  fetchId(movieId).then(movieData => {
-    console.log(movieData);
-    console.log(movieData.popularity);
-  });
+  //console.log(clickedMovie.target);
+
+  if (clickedMovie.target.matches('img')) {
+    movieId = clickedMovie.target.parentNode.id;
+    //console.log(movieId);
+    fetchId(movieId).then(movieData => {
+      modalImg.setAttribute('src', `https://image.tmdb.org/t/p/w500/${movieData.poster_path}`);
+      modalTitle.textContent = movieData.title;
+      modalAbout.textContent = movieData.overview;
+      // console.log(movieData);
+    });
+    toggleModal();
+  } else return;
 }
 
 async function fetchId(movieId) {
@@ -22,4 +38,23 @@ async function fetchId(movieId) {
   }
 }
 
+function toggleModal() {
+  modalBackdrop.classList.toggle('is-hidden');
+}
+function saveToWatched() {
+  fetchId(movieId).then(data => {
+    watchedFilmsArray.push(data);
+    localStorage.setItem('watched-films', JSON.stringify(watchedFilmsArray));
+  });
+}
+function saveToQueue() {
+  fetchId(movieId).then(data => {
+    queuedFilmsArray.push(data);
+    localStorage.setItem('queued-films', JSON.stringify(queuedFilmsArray));
+  });
+}
+
 gallery.addEventListener('click', detailsHandler);
+btnClose.addEventListener('click', toggleModal);
+btnWatched.addEventListener('click', saveToWatched);
+btnQueued.addEventListener('click', saveToQueue);
